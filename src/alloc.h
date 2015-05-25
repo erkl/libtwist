1c88cdde87e0660a3d45aaf0783a5104197bdb1f
Erik Lundin <erik@kvick.co>
2015-05-25 18:38:12 +0100

    Create alloc.h
    This is the first step towards removing support for socket-specific
    allocators. Detecting memory leaks in tests should be simple enough
    anyway, and linking against a custom allocator like jemalloc is a
    breeze.

---
+++ src/alloc.h
@@ -0,0 +1,28 @@
+/* Copyright (c) 2015, Erik Lundin.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE. */
+
+#ifndef LIBTWINE_ALLOC_H
+#define LIBTWINE_ALLOC_H
+
+#include <stdlib.h>
+
+
+/* This would be where to swap the default memory management functions,
+ * if you were so inclined. */
+#define malloc   malloc
+#define realloc  realloc
+#define free     free
+
+
+#endif
