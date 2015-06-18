9fa5bed7e9e240c38baec0a1003e44c0f3a2c910
Erik Lundin <erik@kvick.co>
2015-06-18 22:59:32 +0200

    Prune unused includes from twine.h

--- include/twine.h
+++ include/twine.h
@@ -15,14 +15,10 @@
 #ifndef LIBTWINE_H
 #define LIBTWINE_H
 
-#include <limits.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-
-#include <nectar.h>
 
 
 /* Error codes. */
